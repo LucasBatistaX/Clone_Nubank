8389815713a6ed701a2fd0bd19175cb92e097623
Lucas Batista <Lucas915706@gmail.com>
2025-12-13 13:35:39 -0300

    Header com boas praticas

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  //Colors App
+  //Colors
   static const Color primary = Color(0xFF8A19D6);
   static const Color secundary = Color(0xFFF0F1F5);
 
   //Background
-  static const Color background = Color(0xFFF0F1F5);
   static const Color backgroundAvatar = Color.fromARGB(255, 155, 75, 216);
 }
---
+++ lib/utils/app_sizes.dart
@@ -0,0 +1,9 @@
+class AppSizes {
+  //sizes
+  static const double s24 = 24.00;
+  static const double s28 = 28.00;
+
+  //specific padding
+  static const double p53 = 53.00; //Padding top of the header.
+  static const double h188 = 188.00; //Height of the header container.
+}
